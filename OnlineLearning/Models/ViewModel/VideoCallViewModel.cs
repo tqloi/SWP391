6a6e9432d9808979947931af68401e73a4abac15
thaiphuc1 <thaiphuc6104@gmail.com>
2024-10-23 15:38:38 +0700

    video call

---
+++ OnlineLearning/Models/ViewModel/VideoCallViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineLearning.Models.ViewModel
+{
+    public class VideoCallViewModel
+    {
+        public string Token { get; set; }
+        public string SendId { get; set; }
+        public string ReceiveId { get; set; }
+    }
+}
---
+++ OnlineLearning/Services/StringeeService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace OnlineLearning.Services
+{
+    public class StringeeService
+    {
+        private readonly IConfiguration _configuration;
+
+        public StringeeService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAccessToken(string userId)
+        {
+            var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            var exp = now + 3600 * 24;
+            var apiSid = _configuration["Stringee:ApiSid"];
+            var apiSecret = _configuration["Stringee:ApiSecret"];
+            var header = new JwtHeader(
+                new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(apiSecret)),
+                    SecurityAlgorithms.HmacSha256
+                )
+            );
+
+          //  header["cty"] = _stringeeOption.Cty;
+
+            var payload = new JwtPayload
+    {
+        { "jti", $"{apiSid}-{now}" },
+        { "iss", apiSid },
+        { "exp", exp },
+        { "userId", userId }
+    };
+
+            var jwt = new JwtSecurityToken(header, payload);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenString = tokenHandler.WriteToken(jwt);
+
+            return tokenString;
+        }
+    }
+}
