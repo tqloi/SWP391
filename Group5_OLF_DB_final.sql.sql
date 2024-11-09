2a981b32f1d588aca46c5d5aa62bc4ef544fb36f
tqloi <quyloivn@gmail.com>
2024-11-09 15:44:35 +0700

    fix-modal

--- Group5_OLF_DB_final.sql.sql
+++ Group5_OLF_DB_final.sql.sql
@@ -3,6 +3,87 @@ GO
 USE Online_Learning_Platform;
 GO
 
+--  ASP.NET Identity --
+CREATE TABLE [AspNetUsers] (
+    [Id] NVARCHAR(450) PRIMARY KEY,
+    [UserName] NVARCHAR(256) NULL,
+    [NormalizedUserName] NVARCHAR(256) NULL UNIQUE,
+    [Email] NVARCHAR(256) NULL,
+    [NormalizedEmail] NVARCHAR(256) NULL,
+    [EmailConfirmed] BIT NOT NULL,
+    [PasswordHash] NVARCHAR(MAX) NULL,
+    [SecurityStamp] NVARCHAR(MAX) NULL,
+    [ConcurrencyStamp] NVARCHAR(MAX) NULL,
+    [PhoneNumber] NVARCHAR(15) NULL,
+    [PhoneNumberConfirmed] BIT NOT NULL,
+    [TwoFactorEnabled] BIT NOT NULL,
+    [LockoutEnd] DATETIMEOFFSET NULL,
+    [LockoutEnabled] BIT NOT NULL,
+    [AccessFailedCount] INT NOT NULL,
+    [Address] NVARCHAR(256) NULL,
+    [Dob] DATE NULL,
+    [Gender] NVARCHAR(50) NULL,
+    [FirstName] NVARCHAR(50) NULL,
+    [LastName] NVARCHAR(50) NULL,
+    [WalletUser] DECIMAL(18, 2) NULL,
+	[ProfileImagePath] NVARCHAR(MAX) NULL
+);
+
+CREATE TABLE [AspNetRoles] (
+    [Id] NVARCHAR(450) PRIMARY KEY,
+    [Name] NVARCHAR(256) NULL,
+    [NormalizedName] NVARCHAR(256) NULL UNIQUE,
+    [ConcurrencyStamp] NVARCHAR(MAX) NULL
+);
+INSERT INTO [AspNetRoles] (Id, [Name], [NormalizedName], [ConcurrencyStamp])
+VALUES 
+    ('1', 'Admin', 'ADMIN', NEWID()),
+    ('2', 'Student', 'STUDENT', NEWID()),
+	('3', 'Instructor', 'INSTRUCTOR', NEWID());
+
+CREATE TABLE [AspNetUserRoles] (
+    [UserId] NVARCHAR(450) NOT NULL,
+    [RoleId] NVARCHAR(450) NOT NULL,
+    PRIMARY KEY ([UserId], [RoleId]),
+    FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]) ON DELETE CASCADE,
+    FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles]([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserClaims] (
+    [Id] INT IDENTITY(1,1) PRIMARY KEY,
+    [UserId] NVARCHAR(450) NOT NULL,
+    [ClaimType] NVARCHAR(MAX) NULL,
+    [ClaimValue] NVARCHAR(MAX) NULL,
+    FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserLogins] (
+    [LoginProvider] NVARCHAR(450) NOT NULL,
+    [ProviderKey] NVARCHAR(450) NOT NULL,
+    [ProviderDisplayName] NVARCHAR(MAX) NULL,
+    [UserId] NVARCHAR(450) NOT NULL,
+    PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserTokens] (
+    [UserId] NVARCHAR(450) NOT NULL,
+    [LoginProvider] NVARCHAR(450) NOT NULL,
+    [Name] NVARCHAR(450) NOT NULL,
+    [Value] NVARCHAR(MAX) NULL,
+    PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetRoleClaims] (
+    [Id] INT IDENTITY(1,1) PRIMARY KEY,
+    [RoleId] NVARCHAR(450) NOT NULL,
+    [ClaimType] NVARCHAR(MAX) NULL,
+    [ClaimValue] NVARCHAR(MAX) NULL,
+    FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles]([Id]) ON DELETE CASCADE
+);
+--  ASP.NET Identity - end --
+
 -- instructor table
 CREATE TABLE Instructors (
     InstructorID nvarchar(450) PRIMARY KEY,
@@ -23,7 +104,7 @@ CREATE TABLE [Notification] (
 CREATE TABLE InstructorConfirmation(
 	 ConfirmationID INT PRIMARY KEY IDENTITY(1,1),
 	 UserID nvarchar(450),
-	 Certificatelink nvarchar(400),
+	 Certificatelink nvarchar(MAX),
 	 [FileName] NVARCHAR(255),
 	 SendDate DATETIME DEFAULT GETDATE(),
 	 [Description] NVARCHAR(255),
@@ -153,6 +234,7 @@ CREATE TABLE LectureFiles (
     [FileName] NVARCHAR(255),
 	FileType NVARCHAR(255) CHECK (FileType IN ('Document', 'Video')),
     FilePath NVARCHAR(MAX),  
+	FileExtension NVARCHAR(20),
     UploadDate DATETIME DEFAULT GETDATE(),  
     FOREIGN KEY (lectureID) REFERENCES Lecture(lectureID) ON DELETE CASCADE  
 );
@@ -258,16 +340,6 @@ CREATE TABLE [Message] (
     FOREIGN KEY (senderID) REFERENCES AspNetUsers(Id),  -- Can be a Student or Instructor
 );
 
---MessageFile
-CREATE TABLE MessageFile (
-    FileID INT PRIMARY KEY IDENTITY(1,1),
-    MessageID INT,  
-    [FileName] NVARCHAR(255),  
-    FilePath NVARCHAR(MAX),  
-    UploadDate DATETIME DEFAULT GETDATE(),  
-    FOREIGN KEY (messageID) REFERENCES [Message](Id) ON DELETE CASCADE  
-);
-
 -- Comment table
 CREATE TABLE Comment (
     CommentID INT PRIMARY KEY IDENTITY(1,1),
@@ -324,80 +396,42 @@ CREATE TABLE VideoCallInfo (
     FOREIGN KEY (ReceiveID) REFERENCES AspNetUsers(id)
 );
 go
-alter table VideoCallInfo add CreateAt Datetime;
 
-INSERT INTO Category (FullName, [Description]) 
-VALUES
-    ('Programming', 'Courses related to programming and software development.'),
-    ('Data Science', 'Courses focused on data analysis and machine learning.'),
-    ('Web Development', 'Courses for building websites and web applications.'),
-    ('Design', 'Courses for graphic design and multimedia.'); 
+CREATE TABLE LivestreamRecord 
+(
+	LivestreamRecordID INT PRIMARY KEY IDENTITY(1,1),
+	Title NVARCHAR(255),
+	LivestreamId NVARCHAR(255),
+	UserID NVARCHAR(450),
+	CreateDate DATETIME,
+	UpdateDate DATETIME,
+	CourseID INT,
+	FOREIGN KEY (UserID) REFERENCES AspNetUsers(id),
+	FOREIGN KEY (CourseID) REFERENCES Courses(CourseID) ON DELETE CASCADE
+);
 
 INSERT INTO Courses 
     (Title, CourseCode, [Description], CoverImagePath, InstructorID, NumberOfStudents, Price, CategoryID, [Level], [Status], IsBaned, CreateDate, LastUpdate, EndDate, NumberOfRate, Rating) 
 VALUES
     -- Programming Category
-    ('Python for Beginners', 'CS101', 'Learn Python programming from scratch.', '/Images/cover/python.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 100.00, 1, 'Beginner', 1, 0, '2024-09-01', '2024-09-20', '2024-12-20', 0, NULL),
-    ('Java Advanced Techniques', 'CS102', 'Explore advanced Java programming concepts.', '/Images/cover/java.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 120.00, 1, 'Advanced', 1, 0, '2024-09-05', '2024-09-21', '2024-12-20', 0, NULL),
-    ('C# for Beginners', 'CS103', 'Introduction to C# programming.', '/Images/cover/csharp.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 90.00, 1, 'Beginner', 1, 0, '2024-09-10', '2024-09-22', '2024-12-20', 0, NULL),
-    ('Full-Stack Development with Node.js', 'CS104', 'Learn full-stack development using Node.js.', '/Images/cover/nodejs.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 150.00, 1, 'Intermediate', 1, 0, '2024-09-15', '2024-09-23', '2024-12-20', 0, NULL),
-    ('Introduction to Algorithms', 'CS105', 'Learn about algorithms and data structures.', '/Images/cover/algorithms.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 110.00, 1, 'Advanced', 1, 0, '2024-09-20', '2024-09-24', '2024-12-20', 0, NULL),
+    ('Python for Beginners', 'CS101', 'Learn Python programming from scratch.', '/Images/cover/python.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 100.00, 1, 'Beginner', 1, 0, '2024-09-01', '2024-09-20', '2024-12-20', 0, 0),
+    ('Java Advanced Techniques', 'CS102', 'Explore advanced Java programming concepts.', '/Images/cover/java.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 120.00, 1, 'Advanced', 1, 0, '2024-09-05', '2024-09-21', '2024-12-20', 0, 0),
+    ('C# for Beginners', 'CS103', 'Introduction to C# programming.', '/Images/cover/csharp.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 90.00, 1, 'Beginner', 1, 0, '2024-09-10', '2024-09-22', '2024-12-20', 0, 0),
+    ('Full-Stack Development with Node.js', 'CS104', 'Learn full-stack development using Node.js.', '/Images/cover/nodejs.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 150.00, 1, 'Intermediate', 1, 0, '2024-09-15', '2024-09-23', '2024-12-20', 0, 0),
+    ('Introduction to Algorithms', 'CS105', 'Learn about algorithms and data structures.', '/Images/cover/algorithms.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 110.00, 1, 'Advanced', 1, 0, '2024-09-20', '2024-09-24', '2024-12-20', 0, 0),
 
     -- Data Science Category
-    ('Data Analysis with R', 'DS101', 'Learn data analysis using R programming.', '/Images/cover/data_analysis.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 130.00, 2, 'Beginner', 1, 0, '2024-09-01', '2024-09-20', '2024-12-20', 0, NULL),
-    ('Machine Learning A-Z', 'DS102', 'Master machine learning algorithms and techniques.', '/Images/cover/machine_learning.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 200.00, 2, 'Advanced', 1, 0, '2024-09-05', '2024-09-21', '2024-12-20', 0, NULL),
-    ('Data Science for Everyone', 'DS103', 'Introduction to data science concepts.', '/Images/cover/data_science.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 100.00, 2, 'Beginner', 1, 0, '2024-09-10', '2024-09-22', '2024-12-20', 0, NULL),
-    ('Deep Learning with TensorFlow', 'DS104', 'Explore deep learning with TensorFlow.', '/Images/cover/deep_learning.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 150.00, 2, 'Intermediate', 1, 0, '2024-09-15', '2024-09-23', '2024-12-20', 0, NULL),
-    ('Statistics for Data Science', 'DS105', 'Learn statistics for data science applications.', '/Images/cover/statistics.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 120.00, 2, 'Intermediate', 1, 0, '2024-09-20', '2024-09-24', '2024-12-20', 0, NULL),
+    ('Data Analysis with R', 'DS101', 'Learn data analysis using R programming.', '/Images/cover/data_analysis.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 130.00, 2, 'Beginner', 1, 0, '2024-09-01', '2024-09-20', '2024-12-20', 0, 0),
+    ('Machine Learning A-Z', 'DS102', 'Master machine learning algorithms and techniques.', '/Images/cover/machine_learning.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 200.00, 2, 'Advanced', 1, 0, '2024-09-05', '2024-09-21', '2024-12-20', 0, 0),
+    ('Data Science for Everyone', 'DS103', 'Introduction to data science concepts.', '/Images/cover/data_science.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 100.00, 2, 'Beginner', 1, 0, '2024-09-10', '2024-09-22', '2024-12-20', 0, 0),
+    ('Deep Learning with TensorFlow', 'DS104', 'Explore deep learning with TensorFlow.', '/Images/cover/deep_learning.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 150.00, 2, 'Intermediate', 1, 0, '2024-09-15', '2024-09-23', '2024-12-20', 0, 0),
+    ('Statistics for Data Science', 'DS105', 'Learn statistics for data science applications.', '/Images/cover/statistics.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 120.00, 2, 'Intermediate', 1, 0, '2024-09-20', '2024-09-24', '2024-12-20', 0, 0),
 
     -- Web Development Category
-    ('HTML & CSS for Beginners', 'WD101', 'Learn the basics of web development with HTML & CSS.', '/Images/cover/html_css.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 80.00, 3, 'Beginner', 1, 0, '2024-09-01', '2024-09-20', '2024-12-20', 0, NULL),
-    ('JavaScript Essentials', 'WD102', 'Get started with JavaScript for web development.', '/Images/cover/javascript.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 100.00, 3, 'Beginner', 1, 0, '2024-09-05', '2024-09-21', '2024-12-20', 0, NULL),
-    ('React for Beginners', 'WD103', 'Build user interfaces with React.', '/Images/cover/react.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 150.00, 3, 'Intermediate', 1, 0, '2024-09-10', '2024-09-22', NULL, 0, NULL),
-    ('Node.js for Beginners', 'WD104', 'Learn how to build web applications using Node.js.', '/Images/cover/nodejs_web.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 120.00, 3, 'Intermediate', 1, 0, '2024-09-15', '2024-09-23', '2024-12-20', 0, NULL),
-    ('Advanced CSS Techniques', 'WD105', 'Explore advanced techniques in CSS for modern web design.', '/Images/cover/advanced_css.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 130.00, 3, 'Advanced', 1, 0, '2024-09-20', '2024-09-24', '2024-12-20', 0, NULL);
-
+    ('HTML & CSS for Beginners', 'WD101', 'Learn the basics of web development with HTML & CSS.', '/Images/cover/html_css.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 80.00, 3, 'Beginner', 1, 0, '2024-09-01', '2024-09-20', '2024-12-20', 0, 0),
+    ('JavaScript Essentials', 'WD102', 'Get started with JavaScript for web development.', '/Images/cover/javascript.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 100.00, 3, 'Beginner', 1, 0, '2024-09-05', '2024-09-21', '2024-12-20', 0, 0),
+    ('React for Beginners', 'WD103', 'Build user interfaces with React.', '/Images/cover/react.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 150.00, 3, 'Intermediate', 1, 0, '2024-09-10', '2024-09-22', '2024-12-20', 0, 0),
+    ('Node.js for Beginners', 'WD104', 'Learn how to build web applications using Node.js.', '/Images/cover/nodejs_web.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 120.00, 3, 'Intermediate', 1, 0, '2024-09-15', '2024-09-23', '2024-12-20', 0, 0),
+    ('Advanced CSS Techniques', 'WD105', 'Explore advanced techniques in CSS for modern web design.', '/Images/cover/advanced_css.jpg', 'd6483b30-cacb-43f8-9a96-45c9d8acaca5', 0, 130.00, 3, 'Advanced', 1, 0, '2024-09-20', '2024-09-24', '2024-12-20', 0, 0);
 	-- sua id instructor
-
-ALTER TABLE AspNetUsers
-ALTER COLUMN ProfileImagePath NVARCHAR(MAX);
-
-ALTER TABLE InstructorConfirmation 
-ALTER COLUMN Certificatelink NVARCHAR(MAX);
-
-ALTER TABLE Courses 
-ALTER COLUMN CoverImagePath NVARCHAR(MAX);
-
-ALTER TABLE CourseMaterials 
-ALTER COLUMN MaterialsLink NVARCHAR(MAX);
-
-ALTER TABLE Assignment 
-ALTER COLUMN AssignmentLink NVARCHAR(MAX);
-
-ALTER TABLE CommentFile 
-ALTER COLUMN FilePath NVARCHAR(MAX);
-
-ALTER TABLE LectureFiles 
-ALTER COLUMN FilePath NVARCHAR(MAX);
-
-ALTER TABLE MessageFile 
-ALTER COLUMN FilePath NVARCHAR(MAX);
-
-ALTER TABLE Submission 
-ALTER COLUMN SubmissionLink NVARCHAR(MAX);
-
-Alter table InstructorConfirmation
-Add [FileName] NVARCHAR(255);
-
-ALTER TABLE LectureFiles 
-ADD FileExtension NVARCHAR(20);
-
-ALTER TABLE CourseMaterials 
-ADD FileExtension NVARCHAR(20);
-
-ALTER TABLE AspNetUsers
-ADD [WalletUser] float;
-
-
-
 
